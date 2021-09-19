51bdac9419abb105767af133f124228dfb93ff33
Navya Arora <n22arora@uwaterloo.ca>
2021-09-19 07:48:31 -0400

    changes added

--- src/pages/KnowMore.vue
+++ src/pages/KnowMore.vue
@@ -10,7 +10,7 @@
         <h4 class="title">Amy Santiago
           <br>
           <a href="#">
-            <small>New York, US - 38</small>
+            <small>Age: 38</small>
           </a>
         </h4>
       </div>
@@ -22,15 +22,19 @@
     <div class="row">
       <div class="col" align="center"> 
         <h5 class="title"> Fun Facts</h5>
+          I love reading terms and conditions of a contract<br>
+          I don't like changing lanes without giving signal<br>
+          I like to arrange things into tabs and put margins<br>
+          I like the smell of an old binder<br>
       </div>
       <div class="col" align="center"> 
         <h5 class="title"> How I ended up here?</h5>
+        I got shot in the leg then I coudn't serve for NYPD anymore. The money I saved up could help me with expenses for another 3 yrs. Then, I ended up being homeless after that money exhausted. 
       </div>
     </div>
     <hr>
     <div class="text-center">
-      <p-button type="success" round>Donate Now</p-button>&nbsp;
-      <router-link to ="/knowmore"><p-button type="info" round>Know More</p-button></router-link>
+      <p-button type="success" round>Donate Now</p-button>
     </div>
 </card>
 </template>
